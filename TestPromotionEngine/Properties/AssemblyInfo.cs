3b1daef0801196d65a8dfe66d652fd02e16dcafa
Gaurav Ahuja <gourav17@gmail.com>
2021-05-22 09:25:53 +0530

    Add project files.

---
+++ TestPromotionEngine/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestPromotionEngine")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestPromotionEngine")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("61fcb050-5919-4f03-8cb1-91f426af76cf")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestPromotionEngine/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestPromotionEngine
+{
+    [TestClass]
+    public class TestPromotions
+    {
+        [TestMethod]
+        public void TestPromotionChecker()
+        {
+
+            Assert.AreEqual(0, 0);
+
+        }
+    }
+}
