f2bd62bc39c51b2ac2ecb73ae9e376fe9b2be06e
Gaurav Ahuja <gourav17@gmail.com>
2021-05-22 11:17:10 +0530

    added comments in Test

--- TestPromotionEngine/UnitTest1.cs
+++ TestPromotionEngine/UnitTest1.cs
@@ -12,7 +12,7 @@ namespace TestPromotionEngine
         [TestMethod]
         public void TestPromotionChecker()
         {
-
+            //Creating Products
             Product product1 = new Product("A");
             Product product2 = new Product("A");
             Product product3 = new Product("A");
@@ -21,25 +21,30 @@ namespace TestPromotionEngine
             //Product product2 = new Product("C");
             //Product product3 = new Product("D");
 
+            //Adding products to List
             List<Product> listOfProducts = new List<Product>();
             listOfProducts.Add(product1);
             listOfProducts.Add(product2);
             listOfProducts.Add(product3);
 
+            //Creating Order
             Order order1 = new Order(1, listOfProducts);
 
-            PromotionsList promotionsList = new PromotionsList();
 
+            //Fetching available Promotions.
+            PromotionsList promotionsList = new PromotionsList();
             List<Promotion> promotions = new List<Promotion>();
             promotions = promotionsList.getPromotions();
 
             
+            //Checking if Products in Order is eligible for Promoional Price.
             decimal finalPrice = 0M;
             foreach (Promotion prom in promotions)
             {
                 finalPrice+= PromotionChecker.GetTotalPrice(order1, prom);
             }
 
+//Final Test Results Compare
             Assert.AreEqual(130, finalPrice);
 
 
