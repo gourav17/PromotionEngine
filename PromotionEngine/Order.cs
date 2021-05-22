b9fe6c20dd037ca19938387b98cd367bd350b3b5
Gaurav Ahuja <gourav17@gmail.com>
2021-05-22 10:00:07 +0530

    completed orders class

--- PromotionEngine/Order.cs
+++ PromotionEngine/Order.cs
@@ -8,6 +8,13 @@ namespace PromotionEngine
 {
     public class Order
     {
+        public int OrderID { get; set; }
+        public List<Product> Products { get; set; }
 
+        public Order(int _oid, List<Product> _prods)
+        {
+            this.OrderID = _oid;
+            this.Products = _prods;
+        }
     }
 }
--- PromotionEngine/Product.cs
+++ PromotionEngine/Product.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace PromotionEngine
 {
-    class Product
+   public class Product
     {
     }
 }
