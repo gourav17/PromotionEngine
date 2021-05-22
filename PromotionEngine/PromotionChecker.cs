821b711672cf5ec7ecba09274a31f61883932f94
Gaurav Ahuja <gourav17@gmail.com>
2021-05-22 12:52:58 +0530

    updated

--- PromotionEngine/PromotionChecker.cs
+++ PromotionEngine/PromotionChecker.cs
@@ -17,16 +17,15 @@ namespace PromotionEngine
                 .Select(grp => grp.Count())
                 .Sum();
 
+              
 
-
-            //  var actualPrice = copp * ord.Products
+            
 
             //get count of promoted products from promotion
             int ppc = prom.ProductInfo.Sum(cp => cp.Value);
 
          
 
-            decimal actualPrice =  GetActualPrice(ord.Products);
 
 
             while (copp >= ppc)
@@ -39,45 +38,9 @@ namespace PromotionEngine
                 copp -= ppc;
             }
             
-            return actualPrice - finalPrice;
+            return  finalPrice;
         }
 
-        //Need to improve
-        private static decimal GetActualPrice(List<Product> products)
-        {
-            int counterofA = 0;
-            int priceofA = 50;
-            int counterofB = 0;
-            int priceofB = 30;
-            int CounterofC = 0;
-            int priceofC = 20;
-            int CounterofD = 0;
-            int priceofD = 15;
-            foreach (Product pr in products)
-            {
-                if (pr.Id == "A" || pr.Id == "a")
-                {
-                    counterofA = counterofA + 1;
-                }
-                if (pr.Id == "B" || pr.Id == "b")
-                {
-                    counterofB = counterofB + 1;
-                }
-                if (pr.Id == "C" || pr.Id == "c")
-                {
-                    CounterofC = CounterofC + 1;
-                }
-                if (pr.Id == "D" || pr.Id == "d")
-                {
-                    CounterofD = CounterofD + 1;
-                }
-            }
-            int totalPriceofA = (counterofA % 3 * priceofA);
-            int totalPriceofB = (counterofB % 2 * priceofB);
-            int totalPriceofC = (CounterofC * priceofC);
-            int totalPriceofD = (CounterofD * priceofD);
-            return totalPriceofA + totalPriceofB + totalPriceofC + totalPriceofD;
-
-        }
+     
     }
 }
